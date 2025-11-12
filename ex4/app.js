bece0385fb9d8c84448a40b112dbdd36d4941b76
MrWlobo <michaldworniczak40@gmail.com>
2025-11-12 20:23:35 +0100

    Task 4

--- ex4/app.js
+++ ex4/app.js
@@ -1,6 +1,8 @@
 let lista = document.getElementById("lista");
 let btnAdd = document.getElementById("btnAdd");
 let btnRemove = document.getElementById("btnRemove");
+let btnRemoveNum = document.getElementById("btnRemoveNum");
+let inputNum = document.getElementById("inputNum");
 
 btnAdd.addEventListener("click", () => {
     const li = document.createElement("li");
@@ -15,5 +17,14 @@ btnRemove.addEventListener("click", () => {
         lista.removeChild(lista.firstElementChild);
     } else {
         alert("Lista jest pusta!");
+    }
+});
+
+btnRemoveNum.addEventListener("click", () => {
+    num = inputNum.value;
+    for (i = lista.children.length - 1; i >= 0 ; i--) {
+        if (lista.children[i].textContent.split(" ")[1] == num) {
+            lista.removeChild(lista.children[i])
+        }
     }
 });
